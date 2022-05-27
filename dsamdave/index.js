30d8ec6dd4f1919e293dd9e61d28aadb1d22143d
JIMMY  BOY <105217947+Jimmyobi090@users.noreply.github.com>
2022-05-27 15:13:22 +0100

    Add files via upload

---
+++ dsamdave/index.js
@@ -0,0 +1,20 @@
+// Map, forEach, filter
+
+const students = [
+    {
+        name: "Agail", 
+        score: 27
+    },
+
+    {
+        name: "Abigail", 
+        score: 23, 
+    },
+
+    {
+        name: "bigail", 
+        score: 30
+    }
+]
+
+students.forEach()
